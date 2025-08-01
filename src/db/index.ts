45cd11c2b2414d60f1f2649c54c3adba7d9c71bd
yuvarajmadineni <yuvarajuv49@gmail.com>
2025-08-01 23:34:14 +0530

    fix db env load

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,12 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema.js'
 
-const connectionString = process.env.DATABASE_URL!
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL environment variable is not set')
+}
+
 
 // Create postgres client
 const client = postgres(connectionString)
